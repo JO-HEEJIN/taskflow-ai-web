d96049ec46fa3014bb340cda5ea12adfffd026fb
JO-HEEJIN <midmost44@gmail.com>
2026-01-01 14:56:16 +0900

    Fix mobile scroll by removing overflow-hidden from main container

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h-screen overflow-hidden relative">
+    <main className="min-h-screen relative">
       {/* Background Music Player */}
       <BackgroundMusicPlayer />
 
